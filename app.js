86ba29a2ec96fe1fa707751a455145b501710ed9
Joseph Venuto <jbvenuto@gmail.com>
2019-04-12 09:53:24 -0400

    Added more colors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Variables that are used
-const colors = ["#B2BD7E", "#E9D985", "#749C75", "#6A5D7B", "#5D4A66", "#FFA630", "#4DA1A9", "#2E5077", "#611C35", "#7FB069"];
+const colors = ["#B2BD7E", "#E9D985", "#749C75", "#6A5D7B", "#5D4A66", "#FFA630", "#4DA1A9", "#2E5077", "#611C35", "#7FB069",
+                "#CA3C25", "#E6AA68", "#D56AA0", "#A64253", "#861657", "#F5CAC3", "#083D77", "#2E4057", "#F4D35E", "#F6D8AE",
+                "white", "white", "white", "black", "black", "black", "black", "black",];
 const elements = Math.floor(Math.random() * 42) + 4;
 const x = Math.floor(Math.random() * 23) +3;
 // Variable that will be the HTML code
@@ -7,7 +9,7 @@ let gridHtml = "<div class='grid' style='display:grid;grid-template-columns:";
 
 // Function to select a color
 let colorSelector = () => {
-    let i = Math.floor(Math.random() * 10);
+    let i = Math.floor(Math.random() * 30);
     return i;
 }
 
